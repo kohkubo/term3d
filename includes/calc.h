5bfca975cc67b85c68f1f173b97fafb0c1987afb
kohkubo <kohkubo@student.42tokyo.jp>
2022-02-18 07:12:11 +0900

    Triangleの当たり判定 (#31)
    * feat(三角形の当たり判定):
    
    * refactor: norm対応と高速のためのリファクタリング
    
    * refactor: circleとtriangleを一つのファイルに
    
    * refactor: debugの切り出し、drawの整理
    
    * style: norm対応
    
    * 別のプルリクで投げた変更
    
    * fix: リーク対応
    
    * refactor: 円に合わせたcamera posに変更
    
    * refactor: rotateを関数に切り分け (#35)
    
    * fix: Makefileに依存関係をもたせる
    
    * style: 命名を変更
    
    Co-authored-by: yaito <79784339+yaito6502@users.noreply.github.com>

--- includes/calc.h
+++ includes/calc.h
@@ -1,10 +1,13 @@
-#ifndef CIRCLE_H
-# define CIRCLE_H
+#ifndef CALC_H
+# define CALC_H
 
 # include "data.h"
 # include "vect.h"
 
 t_vect	intersect_with_plane(t_camera *camera, t_vect *center, t_vect *normal);
 bool	is_intersect_with_circle(t_camera *camera, t_circle *circle);
+bool	is_intersect_with_triangle(t_camera *camera, t_triangle *triangle);
+void	rotate_circle(t_circle *circle);
+void	rotate_triangle(t_triangle *triangle);
 
 #endif
---
+++ srcs/calc.c
@@ -0,0 +1,99 @@
+#include "calc.h"
+
+t_vect	intersect_with_plane(t_camera *camera, t_vect *center, t_vect *normal)
+{
+	t_vect	intersect_point;
+	double	t;
+
+	if (is_right_angle(camera->ray, *normal))
+		return (vect_new(DBL_MAX, DBL_MAX, DBL_MAX));
+	t = vect_dot(vect_sub(*center, camera->pos), *normal)
+		/ vect_dot(camera->ray, *normal);
+	if (t < 0)
+		return (vect_new(DBL_MAX, DBL_MAX, DBL_MAX));
+	intersect_point = vect_add(camera->pos, vect_scalar_mul(camera->ray, t));
+	return (intersect_point);
+}
+
+bool	is_intersect_with_circle(t_camera *camera, t_circle *circle)
+{
+	double	distance;
+
+	camera->lookat = intersect_with_plane(camera, &circle->center,
+			&circle->normal);
+	distance = vect_distance(camera->lookat, circle->center);
+	if (less_equal(distance, circle->radius) && \
+less_equal(circle->radius * 0.9, distance))
+		return (true);
+	return (false);
+}
+
+enum
+{
+	U,
+	V,
+	T,
+	DET,
+};
+
+enum
+{
+	P,
+	VT,
+	Q,
+};
+
+/*
+Tomas Mollerのアルゴリズムを使用している。
+カリングが行われるため、この三角形には表と、描画されない裏が存在する。
+高速化とnorm対応のため非常に見にくくなってしまった。
+*/
+bool	is_intersect_with_triangle(t_camera *camera, t_triangle *triangle)
+{
+	t_vect	edge[2];
+	t_vect	vect[3];
+	double	vl[4];
+
+	edge[0] = vect_sub(triangle->vert2, triangle->vert1);
+	edge[1] = vect_sub(triangle->vert3, triangle->vert1);
+	vect[P] = vect_cross(camera->ray, edge[1]);
+	vl[DET] = vect_dot(vect[P], edge[0]);
+	if (less_equal(vl[DET], 0))
+		return (false);
+	vect[VT] = vect_sub(camera->pos, triangle->vert1);
+	vl[U] = vect_dot(vect[P], vect[VT]);
+	if (less(vl[U], 0) || less(vl[DET], vl[U]))
+		return (false);
+	vect[Q] = vect_cross(vect[VT], edge[0]);
+	vl[V] = vect_dot(vect[Q], camera->ray);
+	if (less(vl[V], 0) || less(vl[DET], vl[U] + vl[V]))
+		return (false);
+	vl[T] = vect_dot(vect[Q], edge[1]) / vl[DET];
+	camera->lookat = vect_add(camera->pos, vect_scalar_mul(camera->ray, vl[T]));
+	return (true);
+}
+
+void	rotate_triangle(t_triangle *triangle)
+{
+	double	a;
+
+	triangle->vert1 = vect_rotate(triangle->vert1, vect_new(0, 1, 0),
+			radian(2));
+	triangle->vert2 = vect_rotate(triangle->vert2, vect_new(0, 1, 0),
+			radian(2));
+	triangle->vert3 = vect_rotate(triangle->vert3, vect_new(0, 1, 0),
+			radian(2));
+	a = vect_angle_radian(triangle->vert2, vect_new(0, 0, 1));
+	printf("angle: %f\n", degree(a));
+}
+
+void	rotate_circle(t_circle *circle)
+{
+	double	a;
+
+	circle->normal = vect_rotate(circle->normal, vect_new(0, 1, 0), radian(2));
+	a = vect_angle_radian(circle->normal, vect_new(0, 0, 1));
+	printf("angle: %f\n", degree(a));
+	printf("circle normal ");
+	vect_print(circle->normal);
+}
