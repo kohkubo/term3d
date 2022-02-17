d7117a488cc456dcd3337bf18fe158d31ce19cd3
yaito6502 <schwarzekatze8010110@gmail.com>
2022-02-18 01:29:39 +0900

    feat: 三角形の情報が入った.triファイルを読み込めるようになった。
    .tri フォーマット
    
    [count]
    [vertex1] [vertex2] [vertex3] [normal]
    ... countと同じ数だけ続く

--- gtest/gtest_reader.cpp
+++ gtest/gtest_reader.cpp
@@ -9,8 +9,28 @@ bool is_equal_vector(t_vect *act, t_vect *exp) {
           is_equal(act->z, exp->z));
 }
 
-t_circle new_circle(t_vect center, t_vect normal, double radius)
-{
+void is_equal_circles(t_data data, t_data exp) {
+  EXPECT_EQ(data.count, exp.count);
+  EXPECT_EQ(data.type, exp.type);
+  for (int i = 0; i < data.count; i++) {
+    EXPECT_TRUE(is_equal_vector(&data.circle[i].center, &exp.circle[i].center));
+    EXPECT_TRUE(is_equal_vector(&data.circle[i].normal, &exp.circle[i].normal));
+    EXPECT_TRUE(is_equal(data.circle[i].radius, exp.circle[i].radius));
+  }
+}
+
+void is_equal_triangles(t_data data, t_data exp) {
+  EXPECT_EQ(data.count, exp.count);
+  EXPECT_EQ(data.type, exp.type);
+  for (int i = 0; i < data.count; i++) {
+    EXPECT_TRUE(is_equal_vector(&data.triangle[i].vert1, &exp.triangle[i].vert1));
+    EXPECT_TRUE(is_equal_vector(&data.triangle[i].vert2, &exp.triangle[i].vert2));
+    EXPECT_TRUE(is_equal_vector(&data.triangle[i].vert3, &exp.triangle[i].vert3));
+    EXPECT_TRUE(is_equal_vector(&data.triangle[i].normal, &exp.triangle[i].normal));
+  }
+}
+
+t_circle new_circle(t_vect center, t_vect normal, double radius) {
   t_circle circle;
 
   circle.center = center;
@@ -19,16 +39,14 @@ t_circle new_circle(t_vect center, t_vect normal, double radius)
   return (circle);
 }
 
-void is_equal_circles(t_data data, t_data exp)
-{
-  EXPECT_EQ(data.count, exp.count);
-  EXPECT_EQ(data.type, exp.type);
-  for (int i = 0; i < 100; i++)
-  {
-    EXPECT_TRUE(is_equal_vector(&data.circle[i].center, &exp.circle[i].center));
-    EXPECT_TRUE(is_equal_vector(&data.circle[i].normal, &exp.circle[i].normal));
-    EXPECT_TRUE(is_equal(data.circle[i].radius, exp.circle[i].radius));
-  }
+t_triangle new_triangle(t_vect vert1, t_vect vert2, t_vect vert3, t_vect normal) {
+  t_triangle triangle;
+
+  triangle.vert1 = vert1;
+  triangle.vert2 = vert2;
+  triangle.vert3 = vert3;
+  triangle.normal = normal;
+  return (triangle);
 }
 
 TEST(DISABLED_Reader, InvalidFile) {
@@ -41,7 +59,8 @@ TEST(DISABLED_Reader, InvalidFile) {
 
   // chmod 000が反映されないので、検証不可能
   EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/noperm.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/nopermdir/test.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/nopermdir/test.rt"));
 
   //ディレクトリはfopenで読み込み可能
   EXPECT_TRUE(read_rtfile(&data, "gtest/reader_testfiles/invalid/dir.rt"));
@@ -52,9 +71,12 @@ TEST(DISBALED_Reader, InvalidIdentifier) {
   t_data data = {0};
 
   /*
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/identifier1.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/identifier2.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/identifier3.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/identifier1.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/identifier2.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/identifier3.rt"));
   */
 }
 
@@ -62,14 +84,22 @@ TEST(DISABLED_Reader, InvalidCircleFormat) {
   t_data data = {0};
 
   /*
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/tr_format1.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/tr_format2.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/tr_format3.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/tr_format4.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/tr_format5.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/tr_format6.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/tr_format7.rt"));
-  EXPECT_FALSE(read_rtfile(&data, "gtest/reader_testfiles/invalid/tr_format8.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/tr_format1.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/tr_format2.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/tr_format3.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/tr_format4.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/tr_format5.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/tr_format6.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/tr_format7.rt"));
+  EXPECT_FALSE(read_rtfile(&data,
+  "gtest/reader_testfiles/invalid/tr_format8.rt"));
   */
 }
 
@@ -79,23 +109,50 @@ TEST(Reader, ReadValidCircleFile) {
 
   exp.count = 1;
   exp.type = CIRCLE;
-  exp.circle[0] = new_circle(vect_new(0,0,0), vect_new(1,0,1), 1.0);
+  exp.circle[0] = new_circle(vect_new(0, 0, 0), vect_new(1, 0, 1), 1.0);
   read_rtfile(&data, "gtest/reader_testfiles/valid/case1.cir");
   is_equal_circles(data, exp);
 
   exp.count = 4;
   exp.type = CIRCLE;
-  exp.circle[0] = new_circle(vect_new(0,0,0), vect_new(0,0,1), 1);
-  exp.circle[1] = new_circle(vect_new(0,0,0), vect_new(0,0,1), 2);
-  exp.circle[2] = new_circle(vect_new(0,0,0), vect_new(0,0,1), 3);
-  exp.circle[3] = new_circle(vect_new(0,0,0), vect_new(0,0,1), 4);
+  exp.circle[0] = new_circle(vect_new(0, 0, 0), vect_new(0, 0, 1), 1);
+  exp.circle[1] = new_circle(vect_new(0, 0, 0), vect_new(0, 0, 1), 2);
+  exp.circle[2] = new_circle(vect_new(0, 0, 0), vect_new(0, 0, 1), 3);
+  exp.circle[3] = new_circle(vect_new(0, 0, 0), vect_new(0, 0, 1), 4);
   read_rtfile(&data, "gtest/reader_testfiles/valid/case2.cir");
   is_equal_circles(data, exp);
 
   exp.count = 20;
   exp.type = CIRCLE;
   for (int i = 0; i < exp.count; i++)
-    exp.circle[i] = new_circle(vect_new(0,0,0), vect_new(0,0,1), 1.0);
+    exp.circle[i] = new_circle(vect_new(0, 0, 0), vect_new(0, 0, 1), 1.0);
   read_rtfile(&data, "gtest/reader_testfiles/valid/case3.cir");
   is_equal_circles(data, exp);
+}
+
+TEST(Reader, ReadValidTriagleFile) {
+  t_data data = {0};
+  t_data exp = {0};
+
+  exp.count = 1;
+  exp.type = TRIANGLE;
+  exp.triangle[0] = new_triangle(vect_new(0,2,0), vect_new(2,-2,0), vect_new(-2,-2,0), vect_new(0,0,1));
+  read_rtfile(&data, "gtest/reader_testfiles/valid/case1.tri");
+  is_equal_triangles(data, exp);
+
+  exp.count = 4;
+  exp.type = TRIANGLE;
+  exp.triangle[0] = new_triangle(vect_new(0,2,0), vect_new(2,-2,0), vect_new(-2,-2,0), vect_new(0,0,1));
+  exp.triangle[1] = new_triangle(vect_new(0,2,0), vect_new(2,-2,0), vect_new(-2,-2,0), vect_new(0,0,1));
+  exp.triangle[2] = new_triangle(vect_new(0,2,0), vect_new(2,-2,0), vect_new(-2,-2,0), vect_new(0,0,1));
+  exp.triangle[3] = new_triangle(vect_new(0,2,0), vect_new(2,-2,0), vect_new(-2,-2,0), vect_new(0,0,1));
+  read_rtfile(&data, "gtest/reader_testfiles/valid/case2.tri");
+  is_equal_triangles(data, exp);
+
+  exp.count = 20;
+  exp.type = TRIANGLE;
+  for (int i = 0; i < exp.count; i++)
+    exp.triangle[i] = new_triangle(vect_new(0,2,0), vect_new(2,-2,0), vect_new(-2,-2,0), vect_new(0,0,1));
+  read_rtfile(&data, "gtest/reader_testfiles/valid/case3.tri");
+  is_equal_triangles(data, exp);
 }
--- srcs/reader.c
+++ srcs/reader.c
@@ -13,29 +13,53 @@ void	exit_error(char *errmsg)
 	exit(EXIT_FAILURE);
 }
 
-static void	check_extension(char *filepath)
+static t_type	check_extension(char *filepath)
 {
 	char	*ext;
 
 	if (!filepath)
 		exit_error("does not filepath.");
 	ext = strrchr(filepath, '.');
-	if (!ext || strncmp(ext, ".cir", 5))
+	if (!ext)
 		exit_error("file extension error.");
+	if (!strncmp(ext, ".cir", 5))
+		return (CIRCLE);
+	if (!strncmp(ext, ".tri", 5))
+		return (TRIANGLE);
+	exit_error("file extension error.");
 }
 
 void	read_circle(t_circle *circle, FILE *file)
 {
-	t_vect	nrm;
 	t_vect	cnt;
+	t_vect	nrm;
 	double	radius;
 
 	if (fscanf(file, "%lf,%lf,%lf %lf,%lf,%lf %lf",
 			   &cnt.x, &cnt.y, &cnt.z, &nrm.x, &nrm.y, &nrm.z, &radius) != 7)
-		exit_error("read failed");
+		exit_error("circle object read failed.");
 	circle->center = cnt;
 	circle->normal = nrm;
 	circle->radius = radius;
+}
+
+void	read_triangle(t_triangle *triangle, FILE *file)
+{
+	t_vect	v1;
+	t_vect	v2;
+	t_vect	v3;
+	t_vect	nrm;
+
+	if (fscanf(file, "%lf,%lf,%lf %lf,%lf,%lf %lf,%lf,%lf %lf,%lf,%lf",
+	&v1.x, &v1.y, &v1.z,
+	&v2.x, &v2.y, &v2.z,
+	&v3.x, &v3.y, &v3.z,
+	&nrm.x, &nrm.y, &nrm.z) != 12)
+		exit_error("triangle object read failed.");
+	triangle->vert1 = v1;
+	triangle->vert2 = v2;
+	triangle->vert3 = v3;
+	triangle->normal = nrm;
 }
 
 void	read_rtfile(t_data *data, char *filepath)
@@ -43,16 +67,21 @@ void	read_rtfile(t_data *data, char *filepath)
 	FILE	*file;
 	int		c;
 
-	check_extension(filepath);
+	data->type = check_extension(filepath);
 	file = fopen(filepath, "r");
 	if (!file)
 		exit_perror();
-	data->type = CIRCLE;
 	if (fscanf(file, "%d", &data->count) != 1)
 		exit_error("count read error");
 	c = 0;
 	while (c < data->count)
-		read_circle(&data->circle[c++], file);
+	{
+		if (data->type == CIRCLE)
+			read_circle(&data->circle[c], file);
+		if (data->type == TRIANGLE)
+			read_triangle(&data->triangle[c], file);
+		c++;
+	}
 	if (!feof(file) || ferror(file))
 		exit_error("file error");
 	fclose(file);
