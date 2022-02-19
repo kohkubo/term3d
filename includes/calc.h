187050d17ff665405867828a2b530db799847ad5
kohkubo <kohkubo@student.42tokyo.jp>
2022-02-19 18:29:40 +0900

    feat: 三角形をワイヤーフレーム出力に変更

--- includes/calc.h
+++ includes/calc.h
@@ -7,6 +7,8 @@
 t_vect	intersect_with_plane(t_camera *camera, t_vect *center, t_vect *normal);
 bool	is_intersect_with_circle(t_camera *camera, t_object *object);
 bool	is_intersect_with_triangle(t_camera *camera, t_object *object);
+bool	is_intersect_with_sphere(t_camera *camera, t_vect pos, double radius);
+bool	is_intersect_with_vector(t_camera *camera, t_vect pos, t_vect edge);
 void	rotate_circle(t_object *object);
 void	rotate_triangle(t_object *object);
 
