86ffb75886363f767d94622e4be594bc979594a2
kohkubo <kohkubo@student.42tokyo.jp>
2022-02-20 00:48:49 +0900

    mallocを使わないように変更 (#55)
    * feat(三角形の当たり判定):
    
    * refactor: norm対応と高速のためのリファクタリング
    
    * refactor: circleとtriangleを一つのファイルに
    
    * refactor: debugの切り出し、drawの整理
    
    * style: norm対応
    
    * 別のプルリクで投げた変更
    
    * fix: リーク対応
    
    * feat: 複数circleを描画できるように対応。
    
    #22
    
    * refactor: norm対応
    
    #22
    
    * test: ファイルの内容を変更して、テストが通らなくなったので戻した。
    
    #22
    
    * refactor: 円に合わせたcamera posに変更
    
    * refactor: rotateを関数に切り分け (#35)
    
    * feat: 三角形の情報が入った.triファイルを読み込めるようになった。
    
    .tri フォーマット
    
    [count]
    [vertex1] [vertex2] [vertex3] [normal]
    ... countと同じ数だけ続く
    
    * feat: ファイル読み込みの制約を少し緩く変更。
    
    ファイルの最後に改行を含む、ファイル中に空行がある場合にも正常なファイルとして扱えるようにした。
    
    #36
    
    * fix: #36 不正なファイル入力のテスト追加。それを元に修正。
    
    fscanf()のformatよりも多く読み込んだ場合エラーにしたいができないので、他の読み込み関数を検討する必要がある。
    あまりにも時間をかけてしまったので、とりあえず他を進める。fscanfは正直これが限界かな。
    
    * fix: 消し忘れたdebug出力削除。コンパイルエラー解消。testの一部をコメントアウト。
    
    #36
    
    * refactor: norm対応
    
    #36
    
    * feat: 複数オブジェクトの描画を抽象化
    
    circle,triangleどちらが来ても描画できるようになった。
    
    #36
    
    * feat: さらに抽象化を行った。norm対応。
    
    オブジェクトタイプ判定の条件分岐による計算を極力減らせた。voidポインタと関数ポインタの合わせ技で抽象化。
    
    #36
    
    * test: 未変更だった為コンパイルエラーになったテストケースを修正。.triファイルがまた追加されていなかったので追加。
    
    #36
    
    * test: オブジェクト配列の定数が大きすぎてstackoverflowしたので1/100に修正。
    
    #36
    
    * test:
    
    リモートのCIでfailした部分を修正。
    
    #36
    
    * Update calc.c
    
    * fix: トライアングルの情報受取の部分 (#45)
    
    * refactor: シグニチャーのリファクタリング
    
    * feat: non-block入力受付
    
    * feat: camera move
    
    * feat: triangle 便利関数
    
    * refactor: rotate関数を別ファイルに切り出し
    
    * feat: cameraにnormalをもたせるように変更
    
    * test:
    
    * fix: triangleのバグ修正
    
    * feat: vect_moveの追加
    
    * feat(moveの移動距離の変更):
    
    * feat: 三角形をワイヤーフレーム出力に変更
    
    * refactor: calc系の関数のファイルを切り分け
    
    * build(細かい変更):
    
    * test: テストの追加
    
    * style: norm対応
    
    * feat: mallocを使わないように変更
    
    Co-authored-by: yaito6502 <schwarzekatze8010110@gmail.com>
    Co-authored-by: yaito <79784339+yaito6502@users.noreply.github.com>

--- srcs/debug.c
+++ srcs/debug.c
@@ -4,10 +4,10 @@ void	print_circle_info(t_data *data)
 {
 	double	camera_to_lookat;
 
-	camera_to_lookat = vect_distance(data->camera->pos, data->camera->lookat);
+	camera_to_lookat = vect_distance(data->camera.pos, data->camera.lookat);
 	printf("camera_to_lookat: %f\n", camera_to_lookat);
 	printf("camera pos");
-	vect_print(data->camera->pos);
+	vect_print(data->camera.pos);
 	printf("circle center");
 	vect_print(data->object->pos1);
 }
@@ -17,10 +17,10 @@ void	print_triangle_info(t_data *data)
 	double	camera_to_lookat;
 
 	printf("camera pos    ");
-	vect_print(data->camera->pos);
+	vect_print(data->camera.pos);
 	printf("camera normal ");
-	vect_print(data->camera->normal);
-	camera_to_lookat = vect_distance(data->camera->pos, data->camera->lookat);
+	vect_print(data->camera.normal);
+	camera_to_lookat = vect_distance(data->camera.pos, data->camera.lookat);
 	printf("camera_to_lookat: %f\n", camera_to_lookat);
 	printf("triangle a ");
 	vect_print(data->object->pos1);
