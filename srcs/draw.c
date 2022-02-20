890ec2845dfa2c047ca2243a0a5ca10f003c292c
kohkubo <kohkubo@student.42tokyo.jp>
2022-02-20 14:04:31 +0900

    refactor: レビュー内容を反映

--- srcs/draw.c
+++ srcs/draw.c
@@ -7,7 +7,7 @@ static void	init_data(t_data *data)
 	data->camera.right = vect_new(1, 0, 0);
 	data->camera.normal = vect_new(0, 0, -1);
 	data->camera.normal_axis = vect_normalize(vect_new(1, 1, 1));
-	data->camera.rotate_angle = 0.0174533;
+	data->camera.rotate_angle = radian(1);
 	data->intersect = is_intersect_with_triangle;
 }
 
