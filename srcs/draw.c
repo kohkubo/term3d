cfa1301e2749041c619996bc2285ce4dff6b8777
kohkubo <kohkubo@student.42tokyo.jp>
2022-02-21 22:10:54 +0900

    スクリーンの中心にオブジェクトが来るように変更 (#69)
    * feat: print時にcursorを消すように実装
    
    * feat: オブジェクトを中心に設置するように変更
    
    * refactor: レビュー内容を反映

--- srcs/draw.c
+++ srcs/draw.c
@@ -1,16 +1,5 @@
 #include "draw.h"
 
-static void	init_data(t_data *data)
-{
-	data->camera.pos = vect_new(0, 0, -150);
-	data->camera.up = vect_new(0, 1, 0);
-	data->camera.right = vect_new(1, 0, 0);
-	data->camera.normal = vect_new(0, 0, -1);
-	data->camera.normal_axis = vect_normalize(vect_new(1, 1, 1));
-	data->camera.rotate_angle = radian(1);
-	data->intersect = is_intersect_with_triangle;
-}
-
 static void	draw_point(t_data *data, int x, int y)
 {
 	int		i;
@@ -56,7 +45,6 @@ static void	draw_screen(t_data *data)
 
 void	draw(t_data *data)
 {
-	init_data(data);
 	while (true)
 	{
 		draw_screen(data);
