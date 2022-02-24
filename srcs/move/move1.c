d2c98ea622c6c7303e6ba026969fa0161c67af8a
yaito6502 <schwarzekatze8010110@gmail.com>
2022-02-24 19:03:22 +0900

    perf: 回転の仕方を変更。コードのリファクタリング。

--- srcs/move/move1.c
+++ srcs/move/move1.c
@@ -20,31 +20,51 @@ static int	getch(void)
 	return (ch);
 }
 
+void	other_control(t_camera *camera, int ch)
+{
+	if (ch == 'z')
+		camera_zoom_in(camera);
+	else if (ch == 'x')
+		camera_zoom_out(camera);
+	else if (ch == ' ')
+		camera_stop(camera);
+	else if (ch == 'r')
+		camera_reset(camera);
+	else if (ch == 'q')
+		quit(camera);
+}
+
+void	rotate_control(t_camera *camera, int ch)
+{
+	if (ch == 'i')
+		camera_rotate_up(camera);
+	else if (ch == 'k')
+		camera_rotate_down(camera);
+	else if (ch == 'j')
+		camera_rotate_left(camera);
+	else if (ch == 'l')
+		camera_rotate_right(camera);
+	else if (ch == 'm')
+		camera_rotate_speed_up(camera);
+	else if (ch == 'n')
+		camera_rotate_speed_down(camera);
+	else
+		other_control(camera, ch);
+}
+
 void	key_control(t_camera *camera)
 {
-	int				i;
-	char			ch;
-	const t_control	control[] = {{camera_move_up,			'w'},
-								 {camera_move_down,			's'},
-								 {camera_move_left,			'a'},
-								 {camera_move_right,		'd'},
-								 {camera_rotate_up,			'i'},
-								 {camera_rotate_down,		'k'},
-								 {camera_rotate_left,		'j'},
-								 {camera_rotate_right,		'l'},
-								 {camera_rotate_speed_up,	'm'},
-								 {camera_rotate_speed_down,	'n'},
-								 {camera_zoom_in,			'z'},
-								 {camera_zoom_out,			'x'},
-								 {camera_reset,				'r'},
-								 {camera_stop,				' '},
-								 {quit,						'q'}};
-	const int		count = (sizeof(control) / sizeof(*control));
+	int	ch;
 
 	ch = getch();
-	i = 0;
-	while (i < count && ch != control[i].key)
-		i++;
-	if (i < count)
-		control[i].control(camera);
+	if (ch == 'w')
+		camera_move_up(camera);
+	else if (ch == 's')
+		camera_move_down(camera);
+	else if (ch == 'a')
+		camera_move_left(camera);
+	else if (ch == 'd')
+		camera_move_right(camera);
+	else
+		rotate_control(camera, ch);
 }
--- srcs/move/move3.c
+++ srcs/move/move3.c
@@ -2,20 +2,24 @@
 
 void	camera_rotate_up(t_camera *camera)
 {
-	camera->normal_axis = vect_inv(&camera->right);
+	camera->normal_axis = vect_normalize(\
+	vect_sub(&camera->normal_axis, &camera->right));
 }
 
 void	camera_rotate_down(t_camera *camera)
 {
-	camera->normal_axis = camera->right;
+	camera->normal_axis = vect_normalize(\
+	vect_add(camera->normal_axis, camera->right));
 }
 
 void	camera_rotate_left(t_camera *camera)
 {
-	camera->normal_axis = vect_inv(&camera->up);
+	camera->normal_axis = vect_normalize(\
+	vect_sub(&camera->normal_axis, &camera->up));
 }
 
 void	camera_rotate_right(t_camera *camera)
 {
-	camera->normal_axis = camera->up;
+	camera->normal_axis = vect_normalize(\
+	vect_add(camera->normal_axis, camera->up));
 }
