953e3f568b8d0e27b651c9d5e50ba8aa9ffff314
kohkubo <kohkubo@student.42tokyo.jp>
2022-02-24 13:38:53 +0900

    Update shading.h

--- includes/shading.h
+++ includes/shading.h
@@ -8,7 +8,7 @@
 # define IA 0.1 //環境光の強度
 # define KA 0.01 //環境光反射係数
 # define KD 0.8 //拡散反射係数
-# define KS 0.0 //鏡面反射係数
+# define KS 0.5 //鏡面反射係数
 # define SHININESS 32 //光沢度
 
 double	shading(t_camera *camera, t_light *light, t_object *hit);
