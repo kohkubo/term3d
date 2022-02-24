79d16517bc827975cc1276e497f7d6b8a9182795
yaito6502 <schwarzekatze8010110@gmail.com>
2022-02-24 19:07:00 +0900

    fix: charsetの長さを間違えて測るバグを修正。

--- srcs/config.c
+++ srcs/config.c
@@ -24,11 +24,11 @@ void	set_config(t_data *data, int ac, char **av)
 {
 	data->config.color = BLUE;
 	data->config.charset = "-~=cxFX8NNNNN";
-	data->config.charset_size = strlen(data->config.charset) - 1;
 	if (ac <= 1 || ac >= 5)
 		exit_error("Usage: ./term3d <filename> [color : -rbgymc] [charset]");
 	if (ac >= 3)
 		set_color(&data->config, av[2]);
 	if (ac >= 4)
 		data->config.charset = av[3];
+	data->config.charset_size = strlen(data->config.charset) - 1;
 }
