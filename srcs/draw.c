004ffeb6ca3aa6a36d405106778695687e5d5c9d
yaito6502 <schwarzekatze8010110@gmail.com>
2022-02-24 20:08:31 +0900

    fix: 計算量を減らすために処理を分岐。危ない箇所を修正

--- srcs/draw.c
+++ srcs/draw.c
@@ -4,15 +4,10 @@ extern bool	g_draw_flg;
 
 static void	draw_screen(t_data *data)
 {
-	char	buf[BUFSIZ];
-	int		x;
-	int		y;
+	int	x;
+	int	y;
 
-	setbuf(stdout, buf);
 	printf(TOP_LEFT);
-	printf(DISABLE_CURSOR);
-	printf(BOLD);
-	printf("%s", data->config.color);
 	y = data->camera.height - 1;
 	while (y >= 0)
 	{
@@ -23,16 +18,35 @@ static void	draw_screen(t_data *data)
 		y--;
 	}
 	fflush(stdout);
+}
+
+static void	draw_init(t_data *data, t_thread_line **thread_line)
+{
+	char	buf[BUFSIZ];
+
+	*thread_line = (t_thread_line *)ft_xcalloc(\
+	data->camera.height, sizeof(t_thread_line));
+	setbuf(stdout, buf);
+	printf(DISABLE_CURSOR);
+	printf(BOLD);
+	printf("%s", data->config.color);
+}
+
+static void	draw_end(t_data *data, t_thread_line *thread_line)
+{
 	printf(END);
 	printf(ENABLE_CURSOR);
+	free(thread_line);
+	free(data->canvas);
+	free(data->object);
 }
 
 void	draw(t_data *data)
 {
 	t_thread_line	*thread_line;
 
-	thread_line = (t_thread_line *)ft_xcalloc(\
-	data->camera.height, sizeof(t_thread_line));
+	thread_line = NULL;
+	draw_init(data, &thread_line);
 	while (g_draw_flg)
 	{
 		thread_store_canvas(data, thread_line);
@@ -40,5 +54,5 @@ void	draw(t_data *data)
 		key_control(&data->camera);
 		rotate(data);
 	}
-	free(thread_line);
+	draw_end(data, thread_line);
 }
