69f34cc6a4b532e43fd6d71ecbabbe77f0d9f89e
kohkubo <kohkubo@student.42tokyo.jp>
2022-02-28 10:26:49 +0900

    build: ベンチをついか

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -1,5 +1,7 @@
 #include "rotate.h"
 
+#ifndef BENCH
+
 static void	camera_rotate(t_data *data)
 {
 	data->camera.pos = vect_rotate(&data->camera.pos, \
@@ -11,6 +13,31 @@ static void	camera_rotate(t_data *data)
 	data->base_info.right = vect_rotate(&data->base_info.right, \
 &data->base_info.normal_axis, data->base_info.rotate_angle);
 }
+
+#else
+
+extern bool	g_draw_flg;
+
+static void	camera_rotate(t_data *data)
+{
+	static int	rotate_count = 0;
+
+	data->camera.pos = vect_rotate(&data->camera.pos, \
+&data->base_info.normal_axis, data->base_info.rotate_angle);
+	data->base_info.normal = vect_rotate(&data->base_info.normal, \
+&data->base_info.normal_axis, data->base_info.rotate_angle);
+	data->base_info.up = vect_rotate(&data->base_info.up, \
+&data->base_info.normal_axis, data->base_info.rotate_angle);
+	data->base_info.right = vect_rotate(&data->base_info.right, \
+&data->base_info.normal_axis, data->base_info.rotate_angle);
+	rotate_count++;
+	if (rotate_count == 360)
+	{
+		g_draw_flg = false;
+	}
+}
+
+#endif
 
 void	rotate(t_data *data)
 {
