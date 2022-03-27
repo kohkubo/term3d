631e753117776495fdb5a3e2cda7e7ddb8bf3e6a
yaito <79784339+yaito6502@users.noreply.github.com>
2022-03-28 02:02:05 +0900

    Helpを読みやすくする (#134)
    * fix: ファイル読み込みをgetlineで行うように変更。getlineの仕様を理解する為のテストを追加。
    
    * style: ヘルプを見やすいように修正
    
    * style: norm修正
    
    Co-authored-by: kohkubo <kohkubo@student.42tokyo.jp>

--- srcs/control/getch.c
+++ srcs/control/getch.c
@@ -1,4 +1,28 @@
+#include <fcntl.h>
+
 #include "control.h"
+
+int	fcntl_wrapper(int fd, int cmd, int arg)
+{
+	int ret;
+
+	ret = fcntl(fd, cmd, arg);
+	if (ret == -1)
+		exit_error("fcntl");
+	return (ret);
+}
+
+void	tcgetattr_wrapper(int fd, struct termios *termios)
+{
+	if (tcgetattr(fd, termios) == -1)
+		exit_error("tcgetattr");
+}
+
+void	tcsetattr_wrapper(int fd, int cmd, struct termios *termios)
+{
+	if (tcsetattr(fd, cmd, termios) == -1)
+		exit_error("tcsetattr");
+}
 
 int	getch(void)
 {
@@ -10,11 +34,11 @@ int	getch(void)
 	tcgetattr(STDIN_FILENO, &oldattr);
 	newattr = oldattr;
 	newattr.c_lflag &= ~(ICANON | ECHO);
-	tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
-	old = fcntl(STDIN_FILENO, F_GETFL, 0);
-	fcntl(STDIN_FILENO, F_SETFL, old | O_NONBLOCK);
+	tcsetattr_wrapper(STDIN_FILENO, TCSANOW, &newattr);
+	old = fcntl_wrapper(STDIN_FILENO, F_GETFL, 0);
+	fcntl_wrapper(STDIN_FILENO, F_SETFL, old | O_NONBLOCK);
 	ch = getchar();
-	tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
-	fcntl(STDIN_FILENO, F_SETFL, old);
+	tcsetattr_wrapper(STDIN_FILENO, TCSANOW, &oldattr);
+	fcntl_wrapper(STDIN_FILENO, F_SETFL, old);
 	return (ch);
 }
